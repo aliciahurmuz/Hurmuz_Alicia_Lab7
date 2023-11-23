29c6dc6762315d3a054ce0c3b81b9eed5c9a5897
ALICIA-MARIA HURMUZ <alicia.hurmuz@stud.ubbcluj.ro>
2023-11-23 14:12:52 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Hurmuz_Alicia_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ NewPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace Hurmuz_Alicia_Lab7;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
